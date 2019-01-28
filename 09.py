f332b89a2a441f9fe3657af843ddd7d3be0736b2
Shisei Naka <nathaniel.naka@gmail.com>
2019-01-29 06:47:27 +0900

    solved

--- 09.py
+++ 09.py
@@ -1,8 +1,18 @@
-# スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文（例えば"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."）を与え，その実行結果を確認せよ．
+"""
+スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，
+それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文
+（例えば
+"I couldn't believe that I could actually understand what I was reading :
+the phenomenal power of the human mind ."
+）
+を与え，その実行結果を確認せよ．
+"""
 import random
 
-def typoglycemia(text: str) -> str:
-    """if length of word is grater then 4, shufflethe word except the first and last char
+
+def typoglycemia(text: str):
+    """if length of word is grater then 4, shufflethe word except the first
+    and last char
 
     Parameters
     ----------
@@ -28,6 +38,9 @@ def typoglycemia(text: str) -> str:
         shuffled_text.append(word)
     return ' '.join(shuffled_text)
 
-text = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+text = """
+       I couldn't believe that I could actually understand what I was reading :
+       the phenomenal power of the human mind .
+       """
 
 print(typoglycemia(text))
