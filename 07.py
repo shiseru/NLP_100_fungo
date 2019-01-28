4979bee3d900e41b1fb9967735f22f6b01490c29
Shisei Naka <nathaniel.naka@gmail.com>
2019-01-29 03:25:05 +0900

    solved

--- 07.py
+++ 07.py
@@ -1,6 +1,6 @@
 # 引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．さらに，x=12, y="気温", z=22.4として，実行結果を確認せよ．
 
-def output_phrase(x: int, y: str, z: str):
-    return str(x) + '時の' + y + 'は' + z
+def output_phrase(x, y, z):
+    return '{}時の{}は{}'.format(x, y, z)
 
-print(output_phrase(12, "気温", str(22.4)))
+print(output_phrase(12, "気温", 22.4))
